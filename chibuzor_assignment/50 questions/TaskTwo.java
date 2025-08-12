b004ca3bb0a0ba3afe6ca17d3d940dc77561afe2
Akinpelu Oluwatayo <akinpeluoluwatayo1235@gmail.com>
2025-08-12 07:11:22 +0100

    created a folder

--- chibuzor_assignment/50 questions/TaskTwo.java
+++ chibuzor_assignment/50 questions/TaskTwo.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
-  public class TaskOne{
+  public class TaskTne{
      public static void main(String[] args){
+ 
 
 	Scanner input = new Scanner(System.in);
 
 	System.out.println("Enter Name: ");
 	String userInput = input.nextLine();
 
-	if (userInput.toLowerCase() == "alex"){
+	if (userInput.toLowerCase() == "dog"){
 
-	System.out.println("Hello Friend");
-
+	System.out.println("Woof! Dogs are awesome");
 
 	}else{
 
-	System.out.println("Hi Stranger");
+	System.out.println("Cool choice, but I love dogs");
 
 	}
 
 
 }
-
-
-
-}
+}        
